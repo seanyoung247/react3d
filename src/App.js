191895afd0afec3e38e98ecf94678db57db8683c
sean <seanyoung247@gmail.com>
2023-02-06 19:57:04 +0000

    refactor: Improves object positioning

--- src/App.js
+++ src/App.js
@@ -1,21 +1,23 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react'
 
-import Cuboid from './components/3d/cuboid.js';
-import View3D from './components/3d/view3d.js';
+import Cuboid from './components/3d/cuboid.js'
+import View3D from './components/3d/view3d.js'
 
-import './App.css';
-
+import './App.css'
 
 const camDefault = {
-  x: 0, y: 200, z: 500,
-  rX: 0, rY: 0, rZ: 0,
-  focalLength: 1000
+    x: 0,
+    y: 200,
+    z: 500,
+    rX: 0,
+    rY: 0,
+    rZ: 0,
+    focalLength: 1000,
 }
 
-
 function App() {
-  const [camera, setCamera] = useState(camDefault);
-  useEffect(() => {
+    const [camera, setCamera] = useState(camDefault)
+    useEffect(() => {
     // Nasty prototype code follows...
     const keypress = (e) => {
       switch (e.code) {
@@ -23,140 +25,153 @@ function App() {
         case 'KeyW':
           setCamera({
             ...camera,
-            z: camera.z - 10
-          });
-          break;
+            z: camera.z - 10,
+          })
+          break
         case 'KeyS':
           setCamera({
             ...camera,
-            z: camera.z + 10
-          });
-          break;
+            z: camera.z + 10,
+          })
+          break
         // Move left and right (x-axis)
         case 'KeyA':
-            setCamera({
-              ...camera,
-              x: camera.x - 10
-            });
-            break;
+          setCamera({
+            ...camera,
+            x: camera.x - 10,
+          })
+          break
         case 'KeyD':
           setCamera({
             ...camera,
-            x: camera.x + 10
-          });
-          break;
+            x: camera.x + 10,
+          })
+          break
         // Move up and down (y-axis)
         case 'KeyZ':
           setCamera({
             ...camera,
-            y: camera.y + 10
-          });
-          break;
+            y: camera.y + 10,
+          })
+          break
         case 'KeyX':
           setCamera({
             ...camera,
-            y: camera.y - 10
-          });
-          break;
+            y: camera.y - 10,
+          })
+          break
         // Reset position
         case 'KeyB':
           setCamera({
             ...camera,
-            x: camDefault.x, y: camDefault.y, z: camDefault.z
-          });
-          break;
+            x: camDefault.x,
+            y: camDefault.y,
+            z: camDefault.z,
+          })
+          break
         // Look left right
         case 'KeyQ':
           setCamera({
             ...camera,
-            rY: camera.rY + 0.05
-          });
-          break;
+            rY: camera.rY + 0.05,
+          })
+          break
         case 'KeyE':
           setCamera({
             ...camera,
-            rY: camera.rY - 0.05
-          });
-          break;
+            rY: camera.rY - 0.05,
+          })
+          break
         case 'KeyR':
           setCamera({
             ...camera,
-            rX: camera.rX + 0.05
-          });
-          break;
+            rX: camera.rX + 0.05,
+          })
+          break
         case 'KeyF':
           setCamera({
             ...camera,
-            rX: camera.rX - 0.05
-          });
-          break;
+            rX: camera.rX - 0.05,
+          })
+          break
         case 'KeyT':
           setCamera({
             ...camera,
-            rZ: camera.rZ + 0.05
-          });
-          break;
+            rZ: camera.rZ + 0.05,
+          })
+          break
         case 'KeyY':
           setCamera({
             ...camera,
-            rZ: camera.rZ - 0.05
-          });
-          break;
+            rZ: camera.rZ - 0.05,
+          })
+          break
         // Resets rotation
         case 'KeyV':
           setCamera({
             ...camera,
-            rX: camDefault.rX, rY: camDefault.rY, rZ: camDefault.rZ
-          });
-          break;
+            rX: camDefault.rX,
+            rY: camDefault.rY,
+            rZ: camDefault.rZ,
+          })
+          break
         // Focal length
         case 'KeyO':
           setCamera({
             ...camera,
-            focalLength: camera.focalLength + 10
-          });
-          break;
+            focalLength: camera.focalLength + 10,
+          })
+          break
         case 'KeyL':
           setCamera({
             ...camera,
-            focalLength: camera.focalLength - 10
-          });
-          break;
+            focalLength: camera.focalLength - 10,
+          })
+          break
         case 'KeyP':
           setCamera({
             ...camera,
-            focalLength: camDefault.focalLength
-          });
-          break;
+            focalLength: camDefault.focalLength,
+          })
+          break
+        default:
       }
     }
-    document.addEventListener('keydown', keypress);
-    return () => document.removeEventListener('keydown', keypress);
-  });
+    document.addEventListener('keydown', keypress)
+    return () => document.removeEventListener('keydown', keypress)
+  })
 
   return (
     <>
       <div id="cam-details">
         <h3>Camera</h3>
-        <p>Position x: {camera.x} y: {camera.y} z: {camera.z}</p>
-        <p>Rotation x: {camera.rX.toFixed(2)} y: {camera.rY.toFixed(2)} z: {camera.rZ.toFixed(2)}</p>
+        <p>
+          Position x: {camera.x} y: {camera.y} z: {camera.z}
+        </p>
+        <p>
+          Rotation x: {camera.rX.toFixed(2)} y: {camera.rY.toFixed(2)} z:{' '}
+          {camera.rZ.toFixed(2)}
+        </p>
         <p>Focal Length: {camera.focalLength}</p>
       </div>
       <View3D camera={camera}>
-        <Cuboid position={{x: '0px', y: '100px', z: '0px'}} size={ {w: "150px", h: "100px", d: "200px"} }>
+        <Cuboid
+          position={{ x: '0px', y: '50px', z: '0px' }}
+          size={{ w: '150px', h: '100px', d: '200px' }}
+        >
           {{
-            front: (<div className='cube-side'>Front</div>),
-            back: (<div className='cube-side'>Back</div>),
-            left: (<div className='cube-side'>Left</div>),
-            right: (<div className='cube-side'>Right</div>),
-            top: (<div className='cube-cap'>Top</div>),
-            bottom: (<div className='cube-cap'>Bottom</div>)
+            front: <div className="cube-side">Front</div>,
+            back: <div className="cube-side">Back</div>,
+            left: <div className="cube-side">Left</div>,
+            right: <div className="cube-side">Right</div>,
+            top: <div className="cube-cap">Top</div>,
+            bottom: <div className="cube-cap">Bottom</div>,
           }}
         </Cuboid>
-        <div className='plane'></div>
+        <div className="plane"></div>
       </View3D>
     </>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/components/3d/cuboid.js
+++ src/components/3d/cuboid.js
@@ -2,24 +2,23 @@ import React from 'react';
 import Styles from './cuboid.module.css';
 
 /**
- * Generates a 3D cuboid from html primatives using css transforms
+ * 
+ * @param {*} param0 
+ * @returns 
  */
-export default function Cuboid({position = {x:0, y:0, z:0}, size = {w:"100px", h: "100px", d: "100px"}, children}) 
+export default function Cuboid({position = {x: 0, y: 0, z: 0}, size = {w:"100px", h: "100px", d: "100px"}, children}) 
 {
     const dimensions = {
+        "--x": position.x ?? '0px',
+        "--y": position.y ?? '0px',
+        "--z": position.z ?? '0px',
         "--w": size.w ?? "100px",
         "--h": size.h ?? "100px",
         "--d": size.d ?? "100px"
     };
 
-    const pos = {
-        "--x": position.x ?? '0px',
-        "--y": position.y ?? '0px',
-        "--z": position.z ?? '0px'
-    }
-
     return (
-        <div id={ Styles.cuboid } style={ {...pos, ...dimensions} }>
+        <div id={ Styles.cuboid } style={ dimensions }>
             <div id={ Styles.front } className={ Styles.side }>
                 { children.front }
             </div>
